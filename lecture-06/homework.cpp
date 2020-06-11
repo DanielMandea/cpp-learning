5e31f6f0938af2bdb36702c5cb06917d00301fe5
Theodor Serbana <theodor.serbana@ibm.com>
2020-06-11 15:17:02 +0300

    Adjust lecture06 homework file

--- lecture-06/homework.cpp
+++ lecture-06/homework.cpp
@@ -7,54 +7,7 @@
 class Matrix
 {
 public:
-	Matrix(int n, int m) : mRows{n}, mColumns{m}, mMat{new int*[mColumns]}
-	{
-		for (int i = 0; i < mColumns; ++i)
-		{
-			mMat[i] = new int[mRows];
-		}
-	}
-
-	~Matrix()
-	{
-		for (int i = 0; i < mColumns; ++i)
-		{
-			delete[] mMat[i];
-		}
-
-		delete[] mMat;
-	}
-
-	Matrix(const Matrix& other) : mRows{other.mRows}, mColumns{other.mColumns}, mMat{new int*[mColumns]}
-	{
-		for (int i = 0; i < mColumns; ++i)
-		{
-			mMat[i] = new int[mRows];
-		}
-
-		std::memcpy(other.mMat, mMat, m * n * sizeof(int));
-	}
-
-	void setElementAtPosition(int line, int col, int element)
-	{
-		mMat[line][col] = element;
-	}
-
-	int getElementFromPosition(int line, int col) const
-	{
-		return mMat[line][col];
-	}
-
-
-private:
-	int mRows;
-	int mColumns;
-
-
-
-	//std::vector<std::vector<int>> mMat;
-
-	int** mMat;
+	// implement this class
 };
 
 
